2e1b5ba1aaa46bb94af1a834fd091c1a15d8d2cd
ali41f <alirehman41f@gmail.com>
2019-02-13 02:46:20 -0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello , I'm Ali Rehman</h1>
+    <p>
+      This is minimal vuejs app where you can add skills.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "about",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
